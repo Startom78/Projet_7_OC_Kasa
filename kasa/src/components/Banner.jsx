ec9b8552c74d1a5172f8d4ca201fd305ae886426
Startom78 <tom78630@gmail.com>
2024-04-24 13:44:43 +0200

    finished media querries for 404, mainpage and about

--- kasa/src/components/Banner.jsx
+++ kasa/src/components/Banner.jsx
@@ -1,13 +1,26 @@
 import '../styles/Banner.css'
 
 function Banner({imageUrl, showTitle}) {
-    return (     
-        <div className='banner'>
-            <img src= {imageUrl} alt = " Banniere de Kasa" className='imgBanner'/>
-            {showTitle && <h1> Chez vous, partout et ailleurs </h1>}
-        </div>
     
-    )
+    console.log(window.location.href)
+    if(window.location.href === "http://localhost:3000/about") {
+        return (     
+            <div className='banner about'>
+                <img src= {imageUrl} alt = " Banniere de KasaMMMM" className='imgBanner'/>
+                {showTitle && <h1> Chez vous, partout et ailleurs </h1>}
+            </div>
+        
+        )
+    } 
+    else {
+        return (     
+            <div className='banner'>
+                <img src= {imageUrl} alt = " Banniere de Kasa" className='imgBanner'/>
+                {showTitle && <h1> Chez vous, partout et ailleurs </h1>}
+            </div>
+        
+        )
+    }
 }
 
 export default Banner
--- kasa/src/components/Ratings.jsx
+++ kasa/src/components/Ratings.jsx
@@ -1,13 +1,14 @@
 import { FaStar } from 'react-icons/fa'
+import '../styles/product.css'
 
 const Ratings = ({value}) => {
     const array = new Array(5).fill(1)
     return(
-        <>
-            {array.map((_, index) => <FaStar key={`star-${index}`}
+        <div className='stars'>
+            {array.map((_, index) => <FaStar className = "star" key={`star-${index}`}
                  color= {value >= index + 1 ? "#FF6060" : "#E3E3E3" }
             />)} 
-        </>
+        </div>
     )
 }
 
